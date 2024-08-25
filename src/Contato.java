f5ab39a493ff5d9132a6139157d1632969295d70
bielbarros <gabrielbarrosg11@gmail.com>
2024-08-24 22:57:38 -0300

    Add Contato class with constructor and get method.

---
+++ src/Contato.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Contato {
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Telefone: " + telefone;
+    }
+}
